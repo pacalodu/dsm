fb3261c4112fbb2499246b28ef50cbb1cd96c6e9
Paula Carolina Lozano Duarte <lozano.116348@e.unavarra.es>
2022-03-31 15:58:44 +0200

    Componentes React Native

--- Primeros pasos en React Native/appGaztaroa/App.js
+++ Primeros pasos en React Native/appGaztaroa/App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import Campobase from './componentes/CampobaseComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Pacalodu APP!</Text>
+    <View>
+      <Campobase/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#581845',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
