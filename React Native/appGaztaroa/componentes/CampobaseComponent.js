da161f309a7dc42a5464901ac5a9b3a9ccd69309
Paula Carolina Lozano Duarte <lozano.116348@e.unavarra.es>
2022-04-04 16:30:39 +0200

    Drawer Navigation

--- React Native/appGaztaroa/componentes/CampobaseComponent.js
+++ React Native/appGaztaroa/componentes/CampobaseComponent.js
@@ -5,8 +5,51 @@ import DetalleExcursion from './DetalleExcursionComponent';
 import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Home from './HomeComponent';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function HomeNavegador() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerMode: 'screen',
+        headerTintColor: '#fff',
+        headerStyle: { backgroundColor: '#015afc' },
+        headerTitleStyle: { color: '#fff' },
+      }}
+    >
+      <Stack.Screen
+        name="Homee"
+        component={Home}
+        options={{
+          title: 'Campo Base',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function DrawerNavegador() {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        drawerStyle: {
+          backgroundColor: '#c2d3da',
+        },
+      }}
+    >
+      <Drawer.Screen name="Home" component={HomeNavegador} />
+      <Drawer.Screen name="Calendario" component={CalendarioNavegador} />
+    </Drawer.Navigator>
+  );
+}
+
 
 function CalendarioNavegador() {
   return (
@@ -20,7 +63,7 @@ function CalendarioNavegador() {
       }}
     >
       <Stack.Screen
-        name="Calendario"
+        name="Calendarioo"
         component={Calendario}
         options={{
           title: 'Calendario Gaztaroa',
@@ -43,12 +86,13 @@ class Campobase extends Component {
 
     return (
       <NavigationContainer>
-        <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
-          <CalendarioNavegador />
+        <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
+          <DrawerNavegador />
         </View>
-      </NavigationContainer>   
+      </NavigationContainer>
+
     );
-}
+  }
 }
 
 export default Campobase;
